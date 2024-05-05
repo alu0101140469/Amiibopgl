58832b3f105dc190a7b6d78b69a82d51417ff35c
Daniel Palenzuela Álvarez <Daniel-link@hotmail.com>
2024-05-06 00:04:40 +0100

    5 mayo

---
+++ app/src/main/java/com/example/amiibopgl/pantallas/Inicio.kt
@@ -0,0 +1,140 @@
+package com.example.amiibopgl.pantallas
+
+import android.media.browse.MediaBrowser
+import androidx.annotation.OptIn
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.RawResourceDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.AspectRatioFrameLayout
+import androidx.media3.ui.PlayerView
+import com.example.amiibopgl.R
+
+@OptIn(UnstableApi::class) @Composable
+fun Inicio(navController: NavController) {
+    val context = LocalContext.current
+    val exoPlayer = remember {
+        ExoPlayer.Builder(context).build()
+    }
+
+    exoPlayer.playWhenReady = true
+    exoPlayer.repeatMode = ExoPlayer.REPEAT_MODE_ALL
+    exoPlayer.volume = Float.MIN_VALUE
+
+    val uri = RawResourceDataSource.buildRawResourceUri(R.raw.)
+    val mediaItem = remember {
+        MediaBrowser.MediaItem.Builder()
+            .setUri(uri)
+            .build()
+
+    }
+    exoPlayer.setMediaItem(mediaItem)
+
+    exoPlayer.prepare()
+
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.White,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Image(
+                        painter = painterResource(id = R.drawable.Amiibo_logo),
+                        contentDescription = "Logo",
+                        modifier = Modifier.size(100.dp)
+                    )
+                },
+                actions = {
+                    IconButton(onClick = { navController.navigate("Cuenta") }) {
+                        Icon(
+                            imageVector = Icons.Filled.AccountCircle,
+                            contentDescription = "Perfil de usuario"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior,
+            )
+        },
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = paddingValues.calculateBottomPadding())
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text("Bottom app bar padding:  $paddingValues")
+
+            repeat(50) {
+                Text(it.toString())
+            }
+        }
+        Box(modifier = Modifier.fillMaxSize()) {
+            AndroidView(factory = {
+                PlayerView(it).apply {
+                    useController = false
+                    player = exoPlayer
+                    resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+                }
+            })
+            Column(
+                modifier = Modifier
+                    .width(580.dp)
+                    .height(730.dp)
+                ,
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Button(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = Color.Yellow
+                    ),
+                    onClick = { navController.navigate("") }
+                ) {
+                    Text(text = "Comenzar")
+                }
+            }
+        }
+    }
+}
